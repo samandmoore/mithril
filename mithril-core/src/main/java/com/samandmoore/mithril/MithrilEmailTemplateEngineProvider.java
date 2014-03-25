a419ec52941a8c91eeb92132f871fad237aaef01
Sam Moore <samandmoore@gmail.com>
2014-03-25 17:22:23 -0400

    added a slightly less crummy email template engine service so that this thing can be used without IoC with a little more success

--- mithril-core/src/main/java/com/samandmoore/mithril/MithrilEmailTemplateEngineProvider.java
+++ mithril-core/src/main/java/com/samandmoore/mithril/MithrilEmailTemplateEngineProvider.java
@@ -1,22 +1,56 @@
 package com.samandmoore.mithril;
 
+import java.util.concurrent.ConcurrentMap;
+
+import com.google.common.collect.Maps;
+
 /**
- * NOTE: this shiz is NOT thread-safe. it's just for lazy peeps that don't use IoC
+ * NOTE: this is a gross solution to thread-safety. it's just for lazy peeps that don't use IoC. so, please, use IoC.
  * 
  * @author Sam Moore
  * @since 3/22/14 9:30 PM
  */
 public final class MithrilEmailTemplateEngineProvider {
 
-    private static EmailTemplateEngine DEFAULT_EMAIL_TEMPLATE_ENGINE;
+    private static final EngineRegistry registry = new EngineRegistry();
+
+    private static final Class<EmailTemplateEngine> DEFAULT_EMAIL_TEMPLATE_ENGINE_KEY = EmailTemplateEngine.class;
 
     public static EmailTemplateEngine getDefault() {
 
-        return DEFAULT_EMAIL_TEMPLATE_ENGINE;
+        final EmailTemplateEngine emailTemplateEngine = registry.get(DEFAULT_EMAIL_TEMPLATE_ENGINE_KEY);
+
+        if (emailTemplateEngine != null) {
+
+            return emailTemplateEngine;
+        }
+
+        throw new RuntimeException("No default email template engine configured." +
+                " You must register an engine with the provider before you can create mailers using the default engine.");
     }
 
     public static void setDefault(final EmailTemplateEngine emailTemplateEngine) {
 
-        DEFAULT_EMAIL_TEMPLATE_ENGINE = emailTemplateEngine;
+        registry.register(DEFAULT_EMAIL_TEMPLATE_ENGINE_KEY, emailTemplateEngine);
+    }
+
+    private static final class EngineRegistry {
+
+        private final ConcurrentMap<Class<? extends EmailTemplateEngine>, EmailTemplateEngine> engineMap;
+
+        private EngineRegistry() {
+
+            engineMap = Maps.newConcurrentMap();
+        }
+
+        public <C extends EmailTemplateEngine, T extends EmailTemplateEngine> void register(Class<C> type, T instance) {
+
+            engineMap.put(type, instance);
+        }
+
+        public <C extends EmailTemplateEngine> EmailTemplateEngine get(Class<C> type) {
+
+            return engineMap.get(type);
+        }
     }
 }
